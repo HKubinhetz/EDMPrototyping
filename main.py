374870e986d215527dcea09aa833a90e7cfcc8e1
Henrique Kubinha <rickubinhetz@gmail.com>
2024-02-07 17:27:19 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# This is the step-by-step implementation of a real-world application involving browser and workbook automation.
+
+# --------------------------------------- STEPS -----------------------------------------
+# Step 1) Create a mockup of the real-world workbook.
+# Step 2) Bring BPM automation to the project (Login AND execution)
+# Step 3a) Run the automation from Excel
+# Step 3b) Run the automation WITH parameters
+# Step 4) Record the automation into workbook
+# Step 5) Tests and error handling
+# Step 6) Production
+
+
