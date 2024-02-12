3182ad3c5bb1816e9ca1f06b07bb0ea87d18f6f5
Kubinha <rickubinhetz@gmail.com>
2024-02-12 15:26:12 -0300

    Continuing user interface script.

--- userinterface.py
+++ userinterface.py
@@ -1,5 +1,31 @@
-from tkinter import *
+import PySimpleGUI as sg
 
-tk_variables = Tk()
+# Creating the Layout Object
+layout = [[sg.Text("Olá!! Por favor cadastre-se")],
+          [sg.Text("Insira o e-mail de login"), sg.Input()],
+          [sg.Text("Insira a senha:"), sg.Input(key='Password', password_char="*")],
+          [sg.Text("Insira o link do site:"), sg.Input()],
+          [sg.Button("OK"), sg.Button("Cancelar")]]
 
-tk_variables.mainloop()
+# Main window configuration!
+window = sg.Window("Demo", layout)
+
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if event == "OK" or event == sg.WIN_CLOSED:
+        break
+
+window.close()
+
+
+"""
+print("Arquivo não encontrado! Favor recadastre-se.")
+login = input("Insira o e-mail de login: ")
+password = input("Insira a senha: ")
+website1 = input("Insira o site-base: ")
+website2 = input("Insira o site-suporte (página de abrir processos): ")
+data = {'login': login, 'senha': password, 'site1': website1, 'site2': website2}
+"""
